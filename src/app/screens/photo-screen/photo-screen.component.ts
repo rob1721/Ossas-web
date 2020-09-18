c6e1ccea95ac60dd8daf5946a6323eecf8886030
Roberto Valenzuela <robval.1721@gmail.com>
2020-09-17 22:39:24 -0300

    feat(photo-screen): creado componente photo-screen

---
+++ src/app/screens/photo-screen/photo-screen.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-photo-screen',
+  templateUrl: './photo-screen.component.html',
+  styleUrls: ['./photo-screen.component.css']
+})
+export class PhotoScreenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

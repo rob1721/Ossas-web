0a216df23a233f4b2598e11f3e90db156fa9b12b
Felipe <felipe.wastavino.p@mail.pucv.cl>
2020-09-17 21:06:08 -0300

    feat(app): añadir página home-screen

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeScreenComponent } from './screens/home-screen/home-screen.component';
 import { LoginScreenComponent } from './screens/login-screen/login-screen.component';
 
 const routes: Routes = [
@@ -8,6 +9,10 @@ const routes: Routes = [
     path: 'login',
     component: LoginScreenComponent,
   },
+  {
+    path: 'home',
+    component: HomeScreenComponent,
+  }
 /*  {
     path: '**',
     redirectTo: '/',

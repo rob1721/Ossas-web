703b444fba971c720eede399e79823a1b6554cb9
Roberto Valenzuela <robval.1721@gmail.com>
2020-09-20 04:39:45 -0300

    feat(app): hardcoding para simular la "base de datos"

--- src/app/hardcode.ts
+++ src/app/hardcode.ts
@@ -9,21 +9,60 @@ export class HardCode{
             name: 'User1',
             avatar: 'assets/images/ping.png',
             imgList: null,
-        }
+        },
+        {
+            name: 'User2',
+            avatar: 'assets/images/ping.png',
+            imgList: null,
+        },
+        {
+            name: 'User3',
+            avatar: 'assets/images/ping.png',
+            imgList: null,
+        },
+        {
+            name: 'User4',
+            avatar: 'assets/images/ping.png',
+            imgList: null,
+        },
+        {
+            name: 'User5',
+            avatar: 'assets/images/ping.png',
+            imgList: null,
+        },
+        {
+            name: 'User6',
+            avatar: 'assets/images/ping.png',
+            imgList: null,
+        },
     ];
 
     comments: Commentary[] = [
         {
-            user: {
-                name: 'User1',
-                avatar: 'assets/images/ping.png',
-                imgList: null,
-            },
+            user: this.users[0],
             comment: 'Fea la wea de foto, borrala!'
-        }
+        },
+        {
+            user: this.users[1],
+            comment: 'Fea la wea de foto, borrala!'
+        },
+        {
+            user: this.users[2],
+            comment: 'Fea la wea de foto, borrala!'
+        },
+        {
+            user: this.users[3],
+            comment: 'Fea la wea de foto, borrala!'
+        },
+        {
+            user: this.users[4],
+            comment: 'Fea la wea de foto, borrala!'
+        },
+        {
+            user: this.users[5],
+            comment: 'Fea la wea de foto, borrala!'
+        },
     ];
 
-    constructor() {
-        
-    }
+    constructor() {}
 }

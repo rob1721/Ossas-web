aaa24198b481c4a1f7948f3f040f6e33da020edb
PanchoDon <fmooresiques@gmail.com>
2020-09-29 11:32:57 -0300

    fix(global): Eliminado carousel y navbar sticky

--- src/app/screens/login-screen/login-screen.component.ts
+++ src/app/screens/login-screen/login-screen.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, NgModule, OnInit } from '@angular/core';
+
+
+
+
 
 @Component({
   selector: 'app-login-screen',
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from 'src/app/app-routing.module';
 
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { NavbarInComponent } from './components/navbar-in/navbar-in.component';
+import { NavbarLogComponent } from './components/navbar-log/navbar-log.component';
 
 @NgModule({
   declarations: [
     NavbarComponent,
-    NavbarInComponent
+    NavbarInComponent,
+    NavbarLogComponent
   ],
   imports: [
     CommonModule,
@@ -17,7 +19,8 @@ import { NavbarInComponent } from './components/navbar-in/navbar-in.component';
   ],
   exports: [
     NavbarComponent,
-    NavbarInComponent
+    NavbarInComponent,
+    NavbarLogComponent
   ]
 })
 export class SharedModule { }

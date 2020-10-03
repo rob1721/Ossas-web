ec7a39acb991976125093850ef94c4956ee6e89e
Roberto Valenzuela <robval.1721@gmail.com>
2020-10-02 22:11:40 -0300

    feat(screens): nuevos cambios a profile..segun mockup

--- src/app/screens/profile-screen/profile-screen.component.ts
+++ src/app/screens/profile-screen/profile-screen.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Post } from 'src/app/core/models/post.model';
+import { User } from 'src/app/core/models/user.model';
+import { UserService } from 'src/app/core/services/user/user.service';
 
 @Component({
   selector: 'app-profile-screen',
@@ -6,10 +10,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile-screen.component.css']
 })
 export class ProfileScreenComponent implements OnInit {
+  // @Output() formEmitter: EventEmitter<FormGroup>; // para enviar la info..adelantando pega
+  public editUserForm: FormGroup;
 
-  constructor() { }
+  public userService: UserService;
+  public user: User;
+  public posts: Post[];
+
+  constructor(
+    private formBuilder: FormBuilder,
+    // private editUserService: EditUserService,
+  ) {
+    // this.formEmitter = new EventEmitter<FormGroup()>;
+    this.userService = new UserService();
+    this.user = this.userService.getLoged();
+    this.posts = this.user.posts;
+  }
 
   ngOnInit(): void {
+    // this.buildForm();
   }
+
 
 }

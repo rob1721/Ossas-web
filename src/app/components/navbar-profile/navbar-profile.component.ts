4149d79cc5d9d4b39bd75d065aa3e5b2f54cbf24
Roberto Valenzuela <robval.1721@gmail.com>
2020-10-02 22:09:21 -0300

    feat(components): nueva nvbar para profile

---
+++ src/app/components/navbar-profile/navbar-profile.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/core/models/user.model';
+import { UserService } from 'src/app/core/services/user/user.service';
+
+@Component({
+  selector: 'app-navbar-profile',
+  templateUrl: './navbar-profile.component.html',
+  styleUrls: ['./navbar-profile.component.css']
+})
+export class NavbarProfileComponent implements OnInit {
+
+  public user: User;
+  public userService: UserService;
+
+  constructor() {
+    this.userService = new UserService();
+    this.user = this.userService.getLoged();
+  }
+  ngOnInit(): void {
+  }
+
+
+
+}

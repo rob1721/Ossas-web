51947aeee0a7632264575ba6b604062cc9477fec
Roberto Valenzuela <robval.1721@gmail.com>
2020-10-14 00:21:54 -0300

    feat(services): modelo categoria y like puestos

--- src/app/core/models/post.model.ts
+++ src/app/core/models/post.model.ts
@@ -1,12 +1,14 @@
+import { Category } from './category.model';
 import { Commentary } from './comment.model';
+import { Like } from './like.model';
 
 export interface Post {
     id?: string;
     image: string;
     title: string;
     author: string; // tiene q ser id del user
-    likes: number;
-    category?: string[];
+    likes: number; // likes: Like[];
+    category?: string[]; // arreglo q contiene las id de las categorias // category?: Category[];
     description?: string;
     comments?: Commentary[];
     date: number;

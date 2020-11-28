41c09f7add5401258b07dc4b6f25910ad926e612
Roberto Valenzuela <robval.1721@gmail.com>
2020-11-28 00:24:08 -0300

    feat(post): post's new methods added

--- src/app/core/services/post/post.service.ts
+++ src/app/core/services/post/post.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable, Subject } from 'rxjs';
 import { Post } from '../../models/post.model';
+import { HttpService } from '../http/http.service';
 import { POSTS } from './posts.const';
 
 @Injectable({
@@ -11,7 +12,9 @@ export class PostService {
   private postSelected$: Observable<Post>;
   private postSubject: Subject<Post>;
 
-  constructor() {
+  constructor(
+    private httpService: HttpService
+  ) {
     this.posts = POSTS;
     this.postSubject = new Subject();
     this.postSelected$ = this.postSubject.asObservable();
@@ -31,5 +34,23 @@ export class PostService {
 
   public getPostById(id: string) {
     return this.posts.find((post: Post) => post.id === id);
+  }
+
+
+  // -------------------------------------NEW METHODS
+  public getAllPosts2(): Observable<Post[]> {
+    return this.httpService.get<Post[]>('/post/all');
+  }
+  public getPost2(id: string): Observable<Post>{
+    return this.httpService.get<Post>(`/post/${id}`);
+  }
+  public postPost2(post: Post): Observable<Post>{
+    return this.httpService.post<Post>('/post/', post);
+  }
+  public updatePost2(postId: string, post: Partial<Post>): Observable<Post>{
+    return this.httpService.patch<Post>(`/post/${postId}`, post);
+  }
+  public deletePost2(postId: string): Observable<Post>{
+    return this.httpService.delete<Post>(`/post/${postId}`);
   }
 }

e187a1323c8c4587f37951db21de0df1fe846d29
Felipe <felipe.wastavino.p@mail.pucv.cl>
2020-11-29 19:24:52 -0300

    style(register-screen): Cambio apariencia registro

--- src/app/screens/register-screen/register-screen.component.ts
+++ src/app/screens/register-screen/register-screen.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register-screen',
@@ -7,9 +9,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterScreenComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
-  ngOnInit(): void {
+  bg;
+  bgList;
+
+  ngOnInit() {
+    this.bg = '../../../assets/images/g.jpg';
+    this.bgList = [
+      { 'bg': '../../../assets/images/g.jpg', active: true },
+      { 'bg': '../../../assets/images/a.jpg' },
+      { 'bg': '../../../assets/images/d.jpg' },
+      { 'bg': '../../../assets/images/f.jpg' },
+      { 'bg': '../../../assets/images/h.jpg' },
+      { 'bg': '../../../assets/images/j.jpg' }
+    ];
+  }
+
+  public changeBg(list) {
+    this.bg = list.bg;
+    list.active = true;
+
+    for (let bList of this.bgList) {
+      if (bList != list) {
+        bList.active = false;
+      }
+    }
+  }
+
+  public formSubmit(f: NgForm) {
+    this.router.navigate(['/home']);
   }
 
 }

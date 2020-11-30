10a37d772cedc3548d1280c1b4a7fa97eb24c68c
Felipe <felipe.wastavino.p@mail.pucv.cl>
2020-11-30 01:13:57 -0300

    feat(register-form): Validacion formulario agregado

--- src/app/components/register-form/register-form.component.ts
+++ src/app/components/register-form/register-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-register-form',
@@ -6,10 +7,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register-form.component.css']
 })
 export class RegisterFormComponent implements OnInit {
+  nombre: string = null;
+  email: string = null;
+  pass: string = null;
+  confirmPass: string = null;
 
-  constructor() { }
+  formulario: FormGroup;
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.formulario = this.formBuilder.group({
+      nombre: new FormControl(
+        '',
+        Validators.compose([
+          Validators.required,
+          Validators.pattern('^[a-zA-Z ]+$'),
+          Validators.maxLength(30)
+          // tslint:disable-next-line: max-line-length
+          /* Validators.pattern(/^([0-9]{1,2}[0-9]{6}[0-9kK]{1}\s{0,})$|^[0-9]{1,2}\.\d{3}\.\d{3}[-][0-9kK]{1}\s{0,}$|^([0-9]{1,2}[0-9]{6}-[0-9kK]{1}\s{0,})$/) */
+        ])
+      ),
+      email: new FormControl(
+        '',
+        Validators.compose([
+          Validators.required,
+          Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
+        ])
+      ),
+      pass: new FormControl(
+        '',
+        Validators.compose([
+          Validators.required,
+          Validators.minLength(6)
+        ])
+      ),
+      confirmPass: new FormControl(
+        '',
+        Validators.compose([
+          Validators.required
+        ])
+      ),
+    });
   }
 
+
+    validarDatos(): void{
+      console.log('walala');
+    }
 }
